0321e6260f1ff7046b46c5223097f7dcc7308b4f
Angelica Cruz <brantleyhomebuyer@gmail.com>
2020-04-26 16:48:56 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,8 +5,12 @@ birthday_kids = {
 }
 
 def happy_birthday(birthday_kids)
+  birthday_kids.each do |kids_name, age|
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
+end
+  
   
 # # add your code snippet here!
-end
 
 
